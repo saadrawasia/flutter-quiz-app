16ceb37e4509c16681eb14cf0d71b9c852f6f94f
Saad Rawasia <saad.rawasia15@gmail.com>
2025-03-28 10:29:01 +0100

    Added static questions screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,6 +13,25 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreen extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Questions Screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'This is a question',
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(height: 30),
+          AnswerButton(answerText: 'Answer 1', onTap: () {}),
+          const SizedBox(height: 10),
+          AnswerButton(answerText: 'Answer 2', onTap: () {}),
+          const SizedBox(height: 10),
+          AnswerButton(answerText: 'Answer 3', onTap: () {}),
+          const SizedBox(height: 10),
+          AnswerButton(answerText: 'Answer 4', onTap: () {}),
+        ],
+      ),
+    );
   }
 }
